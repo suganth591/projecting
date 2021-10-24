66d80d8996899f9c5f1fd92ee2bce20776b31e87
suganth591 <81741918+suganth591@users.noreply.github.com>
2021-10-24 16:16:00 +0530

    Update login.js

--- login.js
+++ login.js
@@ -1,2 +1,14 @@
-CORScredentials: "omit";
-hello
+const fs=require('fs');
+fs.open("data.txt","w",(err,file))
+var user;
+var password;
+function check(){
+user=document.getElementById("username").value;
+password=document.getElementById("password").value;
+console.log(user);
+if(user==""||password==""){
+    document.getElementById("inc").style.display="block";
+}else{
+    document.getElementById("inc").style.display="none";
+}
+}
